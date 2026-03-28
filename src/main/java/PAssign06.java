e821bd80119326fdbfd51f5934deb985842e3531
Brian Abbott <hi@brianabbott.com>
2026-03-28 07:04:01 -0400

    add items

---
+++ src/main/java/PAssign06.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * File: PAssign06.java
+ * Class: CSCI 1302
+ * Author: Brian Abbott
+ * Created on: 3/27/26
+ * Last modified: 3/27/26
+ * Description: To be filled in by the user.
+ */
+
+public class PAssign06 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
